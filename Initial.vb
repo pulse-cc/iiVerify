0ffcfe999e5ad8662f1facab83997d9da2cd6131
dymon <obuvalin@gmail.com>
2011-08-18 05:27:51 +0400

    temp

--- Initial.vb
+++ Initial.vb
@@ -40,7 +40,7 @@
 
     Private Sub Initial_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'TODO: данная строка кода позволяет загрузить данные в таблицу "MetrologyDataSet.Enterprize". При необходимости она может быть перемещена или удалена.
-        ' Me.EnterprizeTableAdapter.Fill(Me.MetrologyDataSet.Enterprize)
+        Me.EnterprizeTableAdapter.Fill(Me.MetrologyDataSet.Enterprize)
         Me.Width = 445
         Me.Height = 345
         Me.CenterToScreen()
@@ -126,6 +126,7 @@
             'Me.EnterprizeTableAdapter.NewEnterprize()
             '(Me.MetrologyDataSet.Enterprize)
             'Me.EnterprizeBindingSource.S
+            Me.EnterprizeTableAdapter.Update(Me.MetrologyDataSet.Enterprize)
             My.Settings.Save()
             Me.Close()
         End If
@@ -172,16 +173,19 @@
             PINbox.Visible = False
         End If
     End Sub
+
     Structure EMF
         Dim value As Double
         Dim nameOfDimension As String
     End Structure
+
     Sub showErr()
         If LogErr.Text <> "" Then
             LogErr.Visible = True
             Log.Height = 134
         End If
     End Sub
+
     Sub getEMF(ByVal EMFnum As Int16, ByVal EMFname As String, ByVal refVer As Boolean)
         Dim Head As String = ""
         Dim Tail As String = ""
